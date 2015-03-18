85e1643089ef6dc63ad044ec8c2dff19f4a22dec
Tyler <tyler.rinker@gmail.com>
2015-03-18 00:10:09 -0400

    added build command to basic pax maintence file

--- inst/templates/maintenance.R
+++ inst/templates/maintenance.R
@@ -4,6 +4,11 @@
 devtools::test()
 
 #==========================
+# build
+#==========================
+devtools::document()
+
+#==========================
 # knit README.md
 #==========================
 knitr::knit2html("README.Rmd", output ="README.md"); unlink("README.html", recursive = TRUE, force = FALSE)
