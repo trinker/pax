5f73d638919ef0d169ce7bb402ad57262848acf3
Tyler <tyler.rinker@gmail.com>
2015-03-30 20:00:40 -0400

    Fixed pax_options unit test

--- tests/testthat/test-pax_options.R
+++ tests/testthat/test-pax_options.R
@@ -3,7 +3,7 @@ context("Checking pax_options")
 test_that("pax_options outputs a string of \n separated options",{
 
     expected <- paste(c(
-        "options(name = c(first=\"\",  last=\"\"))",
+        "options(name = c(first = \"\",  last = \"\"))",
         "options(email = \"\")",  
         "options(license = \"\")",
         "options(github.user = \"\")",  
